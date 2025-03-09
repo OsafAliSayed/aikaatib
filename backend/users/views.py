003b41c849aa599ac1e017264617a3754375411f
osafalisayed <aousafalisayedb2@gmail.com>
2025-03-09 15:57:54 +0530

    make register endpoint public

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,10 +1,11 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework import status, permissions
 from .serializers import UserSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 @api_view(['POST'])
+@permission_classes([permissions.AllowAny])
 def register_user(request):
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
